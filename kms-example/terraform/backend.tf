84183247ddf35a085eb8b05b95829b148e6794fc
Song Li <da_tou_li@yahoo.com>
2024-10-19 19:11:05 -0400

    Working on kms-example

---
+++ kms-example/terraform/backend.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "terraform.huge.head.li.2023"
+    dynamodb_table = "terraform-state-lock"
+    key            = "kms-example"
+    region         = "us-east-1"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.72.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+
