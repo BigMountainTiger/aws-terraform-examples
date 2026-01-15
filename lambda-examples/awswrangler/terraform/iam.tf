bc4f8e6c22761bde916a2327c485112d08bad1d6
Song Li <da_tou_li@yahoo.com>
2026-01-15 17:29:55 -0500

    Updated the aws-terraform-examples

--- lambda-examples/awswrangler/terraform/iam.tf
+++ lambda-examples/awswrangler/terraform/iam.tf
@@ -19,3 +19,25 @@ resource "aws_iam_role_policy_attachment" "lambda_execution_role_basic" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy" "lambda_execution_role" {
+  role = aws_iam_role.lambda_execution_role.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          aws_s3_bucket.s3.arn,
+          "${aws_s3_bucket.s3.arn}/*"
+        ]
+      }
+    ]
+  })
+}
