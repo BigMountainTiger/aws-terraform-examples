27aca00064d03c355cd01319a4e47d19a53ebd28
Song Li <da_tou_li@yahoo.com>
2026-03-26 16:48:43 -0400

    Updated the aws-terraform-examples

--- glue-example/glue-failure-notification/terraform/example.tf
+++ glue-example/glue-failure-notification/terraform/example.tf
@@ -1,17 +1,23 @@
-locals {
-  eventbridge_rule_name = "simulated-glue-job-state-change"
-}
+# "SUCCEEDED", "FAILED", "TIMEOUT", "STOPPED"
+# "STOPPED" means the job was manually stopped. It is not an actual failure.
+
+# Note: In real AWS Glue jobs, the source is "aws.glue",
+# we can not put "aws.glue" as source through the AWS cli, so we use "song.example.glue" here for testing purpose.
+
+# If the number of the jobs are not every large, we do not need to filter by jobName.
 
 module "glue_job_state_change_metrics" {
   source = "./modules/glue_job_failure_notification"
 
-  eventbridge_rule_name = local.eventbridge_rule_name
   eventbridge_rule_pattern = jsonencode({
-    # we can not put "aws.glue" as source through the AWS cli
     "source" : ["song.example.glue"],
     "detail-type" : ["Glue Job State Change"],
     "detail" : {
-      "state" : ["SUCCEEDED", "FAILED", "TIMEOUT", "STOPPED"]
+      "state" : ["FAILED", "TIMEOUT"],
+      "jobName" : [
+        { "wildcard" : "simulated-glue-job-*" },
+        { "wildcard" : "simulated-*" }
+      ]
     }
   })
 }
