e6c51ee6b48cb75c39515e885128532c86308202
Song Li <da_tou_li@yahoo.com>
2026-09-14 18:28:29 -0400

    Updated the aws-terraform-examples

--- athena/sql-examples/examples/multi_column_in_condition/2_select.py
+++ athena/sql-examples/examples/multi_column_in_condition/2_select.py
@@ -1,8 +1,11 @@
+import awswrangler as wr
+import pandas as pd
 from athena_client import AthenaClient
 
 s3_bucket = "athena-sql-examples-huge-head-li"
 database = "athena_sql_examples_database"
 table = "multi_column_in_condition"
+filter_table = "multi_column_in_condition_filter"
 
 
 client = AthenaClient(f"s3://{s3_bucket}/athena/")
@@ -44,4 +47,43 @@ df = client.read_sql_query(sql=sql)
 print(df)
 
 
+# 3rd query
+print()
+print('3rd query')
+
+def create_filter_table():
+    data = {
+        "id": [],
+        "name": []
+    }
+
+    for i in range(1, 2 + 1):
+        data["id"].append(i)
+        data["name"].append(f"name {i}")
+
+    df = pd.DataFrame(data=data)
+    df = df.convert_dtypes(dtype_backend="pyarrow")
+
+    wr.s3.to_parquet(
+            df=df,
+            path=f"s3://{s3_bucket}/{database}/{filter_table}",
+            dataset=True,
+            database=database,
+            table=filter_table,
+            mode="overwrite"
+    )
+
+create_filter_table()
+
+sql = f"""
+    select *
+    from {database}.{table}
+    where (id, name) in (
+        select * from {database}.{filter_table}
+    )
+    order by mnt, id;
+"""
+
+df = client.read_sql_query(sql=sql)
+print(df)
 
